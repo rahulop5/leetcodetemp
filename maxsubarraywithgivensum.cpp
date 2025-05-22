60acc5ceaa46e8f7cf69e1a570556672aef5af65
Rahul <venkatrahulxyz@gmail.com>
2025-05-22 23:57:10 +0530

    sum dumass question

---
+++ maxsubarraywithgivensum.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestSubarray(vector<int> &a, int k)
+{
+    int maxi=0;
+    
+
+    return maxi;
+}
+
+int main()
+{
+    vector<int> a={10, 5, 2, 7, 1, 9};
+    int k=15;
+    cout<<longestSubarray(a, k);
+
+    return 0;
+}
